4fbe000accfbbacf1e67b65e0691e6b8924001d6
独木舟 <1978141412@qq.com>
2026-01-20 18:31:27 +0800

    update: 调整所见即所得值格式

--- src/ts/markdown/getHTML.ts
+++ src/ts/markdown/getHTML.ts
@@ -4,7 +4,9 @@ export const getHTML = (vditor: IVditor) => {
     if (vditor.currentMode === "sv") {
         return vditor.lute.Md2HTML(getMarkdown(vditor));
     } else if (vditor.currentMode === "wysiwyg") {
-        return vditor.lute.VditorDOM2HTML(vditor.wysiwyg.element.innerHTML);
+        const markdownText = getMarkdown(vditor);
+        const innnerHTML = vditor.lute.Md2VditorIRDOM(markdownText);
+        return vditor.lute.VditorIRDOM2HTML(innnerHTML);
     } else if (vditor.currentMode === "ir") {
         return vditor.lute.VditorIRDOM2HTML(vditor.ir.element.innerHTML);
     }
