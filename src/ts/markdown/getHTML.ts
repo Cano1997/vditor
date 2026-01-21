bb2f820cd599e066fdff70911887f087ea6472ff
独木舟 <1978141412@qq.com>
2026-01-21 11:08:45 +0800

    update: 调整分屏预览模式下getHTML值模式

--- src/ts/markdown/getHTML.ts
+++ src/ts/markdown/getHTML.ts
@@ -2,7 +2,9 @@ import {getMarkdown} from "./getMarkdown";
 
 export const getHTML = (vditor: IVditor) => {
     if (vditor.currentMode === "sv") {
-        return vditor.lute.Md2HTML(getMarkdown(vditor));
+        const markdownText = getMarkdown(vditor);
+        const innnerHTML = vditor.lute.Md2VditorIRDOM(markdownText);
+        return vditor.lute.VditorIRDOM2HTML(innnerHTML);
     } else if (vditor.currentMode === "wysiwyg") {
         const markdownText = getMarkdown(vditor);
         const innnerHTML = vditor.lute.Md2VditorIRDOM(markdownText);
