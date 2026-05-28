599ddde851068d24f24ca0aa1bd8ae465b83c106
独木舟 <1978141412@qq.com>
2026-05-28 15:28:12 +0800

    update: 代码块统一只读态只预览

--- src/ts/util/code-helper.ts
+++ src/ts/util/code-helper.ts
@@ -1,12 +1,20 @@
 export const handleCodeblock = (htmlString: string) => {
     const template = document.createElement("template");
     template.innerHTML = htmlString;
-    const nodes = template.content.querySelectorAll('div[data-type="code-block"] pre.vditor-ir__preview');
-    nodes.forEach(node => {
-        const codeNode = node.querySelector('code');
-        if (codeNode) {
-            codeNode.innerHTML = '';
+    const container = template.content
+        .querySelectorAll('div[data-type="code-block"]');
+    container.forEach(item => {
+        if (!item.classList.contains("vditor-ir__node--echarts")) {
+            const nodes = item.querySelectorAll(
+                'div[data-type="code-block"] pre.vditor-ir__preview'
+            );
+            nodes.forEach((node) => {
+                const codeNode = node.querySelector("code");
+                if (codeNode) {
+                    codeNode.innerHTML = "";
+                }
+            });
         }
     })
     return template.innerHTML;
-}
+};
